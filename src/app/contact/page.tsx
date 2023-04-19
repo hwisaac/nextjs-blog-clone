59bb28666717f273cc768310e44311a38b0bdfa5
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-04-19 21:30:49 +0900

    ContactForm.tsx, API(Contact)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,31 @@
+import ContactForm from '@/components/ContactForm';
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 
 type Props = {};
-
+const LINKS = [
+  { icon: <AiFillGithub />, url: '' },
+  { icon: <AiFillLinkedin />, url: '' },
+  { icon: <AiFillYoutube />, url: '' },
+];
 export default function ContactPage({}: Props) {
-  return <div>Contact</div>;
+  return (
+    <section className='flex flex-col items-center'>
+      <h2 className='text-3xl font-bold my-2'>Contact Me</h2>
+      <p>wh2ssac@naver.com</p>
+      <ul className='flex gap-4 my-2'>
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target='_blank'
+            className='text-5xl hover:text-yellow-500'>
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className='text-3xl font-bold my-8'>Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
